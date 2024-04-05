a0c7d0ea99acc4d1d156800142642f84e18dfe52
Shae <a.thomas.vc@gmail.com>
2024-04-05 11:31:42 -0700

    Implemented `Grammar`

---
+++ grammar/__init__.py
@@ -0,0 +1,49 @@
+#!/bin/python3
+
+'''Provides the framework for Caustic's grammar management'''
+
+#> Imports
+from collections import abc as cabc
+#</Imports
+
+__all__ = ('nodes', 'patterns', 'Grammar')
+
+#> Package
+from . import nodes # re-exposed
+from . import patterns # re-exposed
+#</Package
+
+#> Header >/
+class Grammar:
+    '''Holds grammar for both nodes and patterns'''
+    __slots__ = ('nodes', 'patterns')
+
+    nodes: dict[str, nodes.GrammarNode]
+    patterns: patterns.PatternLoader
+
+    def __init__(self, *, nodes: dict[str, nodes.GrammarNode | None] = None,
+                 patts: patterns.PatternLoader | type[patterns.PatternLoader] | None = None):
+        self.patterns = (PatternLoader() if patts is None
+                         else patterns() if isinstance(patterns, type) else patterns)
+        self.nodes = ({} if nodes is None else nodes)
+
+    def node_stat(self, nodes: cabc.Sequence[str] | None = None) -> tuple[frozenset[str], frozenset[str]]:
+        '''
+            Returns a tuple of two frozensets,
+                with the first being names of nodes that have no `.failure`,
+                and the second being names of nodes that have `.failure`
+        '''
+        nodes = frozenset(self.nodes.keys() if nodes is None else nodes)
+        succs = frozenset((n for n in nodes if self.nodes[n].failure is None))
+        return (succs, nodes - succs)
+    def compile(self, nodes: cabc.Sequence[str] | None = None) -> frozenset[str]:
+        '''Compiles nodes, returning a frozenset of node names that failed compilation'''
+        nodes = frozenset(self.nodes.keys() if nodes is None else nodes)
+        prev_succ = -1
+        successes = set()
+        while prev_succ != (prev_succ := len(successes)):
+            for n in nodes:
+                self.nodes[n]
+                if self.nodes[n].failure is None:
+                    successes.add(n)
+        return nodes - successes
