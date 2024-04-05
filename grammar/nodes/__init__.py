6d580920e0bbc046c60ab67109d6f426b7019144
Shae <a.thomas.vc@gmail.com>
2024-04-05 12:58:49 -0700

    Implemenetd `UnionNode`

--- grammar/nodes/__init__.py
+++ grammar/nodes/__init__.py
@@ -5,7 +5,6 @@
 #> Imports
 import re
 import typing
-import operator
 import functools
 from abc import ABC, abstractmethod, abstractproperty
 
@@ -76,6 +75,34 @@ class PatternNode(GrammarNode):
 
     def match(self, on: AbstractBufferMatcher) -> tuple[str, re.Match] | None:
         return on(self.patt.match)
+
+class UnionNode(GrammarNode):
+    '''References multiple other nodes in a union, matching one of any of them'''
+    __slots__ = ('failure', 'nodes')
+
+    nodes: dict[str, GrammarNode | None]
+
+    def setup(self, *nodes: str) -> None:
+        self.failure = Exception('Node was never compiled')
+        self.nodes = dict.fromkeys(nodes, None)
+
+    def compile(self) -> None:
+        for node in self.nodes.keys():
+            if node not in self.bound.nodes:
+                self.failure = KeyError(f'Required node {node!r} is missing')
+                return
+            n = self.nodes[node] = self.bound.nodes[node]
+            if n.failure is not None:
+                self.failure = ValueError(f'Required node {node!r} failed to compile')
+                self.failure.__context__ = n.failure
+                return
+        self.failure = None
+
+    def match(self, on: AbstractBufferMatcher) -> dict | None:
+        for name,node in self.nodes.items():
+            if (m := node.match(on)) is not None:
+                return {'name': name, 'val': m}
+        return None
 #</Header
 
 #> Package >/
