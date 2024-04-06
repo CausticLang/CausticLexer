90b5a1f906ad52bd7a8573d76d0da6861100aca7
Shae <a.thomas.vc@gmail.com>
2024-04-06 12:56:49 -0700

    Implemented `NotNode`

--- grammar/nodes/meta.py
+++ grammar/nodes/meta.py
@@ -11,7 +11,7 @@ from ..patterns.buffermatcher import AbstractBufferMatcher
 #</Imports
 
 #> Header >/
-__all__ = ('TrueNode', 'FalseNode')
+__all__ = ('TrueNode', 'FalseNode', 'NotNode')
 
 class TrueNode(GrammarNode):
     '''Always matches, returning `val` without consuming anything'''
@@ -42,3 +42,41 @@ class FalseNode(GrammarNode):
 
     def match(self, on: AbstractBufferMatcher) -> object:
         return self.NOMATCH
+
+class NotNode(GrammarNode):
+    '''
+        Only matches (returning `val`) if the given node does not match
+        Note: backtracks if the given node does match
+    '''
+    __slots__ = ('failure', 'node_name', 'node', 'val')
+
+    ReturnMode: None = None
+    return_mode: None = None
+
+    node_name: str
+    node: GrammarNode | None
+    val: typing.Any
+
+    def setup(self, node: str, val: typing.Any) -> None:
+        self.failure = Exception('Node was never compiled')
+        self.node_name = node
+        if val is self.NOMATCH:
+            raise ValueError('val cannot be NOMATCH')
+        self.val = val
+
+    def compile(self) -> None:
+        if self.node_name not in self.bound.nodes:
+            self.failure = KeyError(f'Required node {node!r} is missing')
+            return
+        self.node = self.bound.nodes[self.node_name]
+        if self.node.failure is not None:
+            self.failure = ValueError(f'Required node {node!r} failed to compile')
+            self.failure.__context__ = self.node.failure
+        self.failure = None
+
+    def match(self, on: AbstractBufferMatcher) -> object | typing.Any:
+        save = on.save_loc()
+        if self.node(on) is self.NOMATCH:
+            return self.val # -> typing.Any
+        on.load_loc(save) # backtrack
+        return self.NOMATCH # -> object
