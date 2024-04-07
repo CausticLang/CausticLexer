fa15e6b8bd29f09384e168c272438fefb72c1438
Shae <a.thomas.vc@gmail.com>
2024-04-07 16:48:33 -0700

    Implemented `Grammar` `.add_node()` and `.pop_node()`

--- grammar/__init__.py
+++ grammar/__init__.py
@@ -58,3 +58,45 @@ class Grammar:
                 else:
                     node.compile_order_hint += 1
         return nodes - successes
+
+    def add_node(self, node: nodes.GrammarNode, /,*, replace: bool = False,
+                 bind: bool = True, compile: bool = True, **kwargs) -> nodes.GrammarNode:
+        '''
+            Adds a node, using its name as its key
+
+            Raises a `NodeExistsError` if the node already exists, unless `replace`
+
+            Binds the node to this `Grammar` with `node.bind()` if `bind`
+
+            If `compile`, then `.compile()` (`Grammar.compile()`, not `node.compile()`)
+                is executed after adding it, with `needed` being true unless the node replaced
+                another node, in which case all nodes should be recompiled in case any
+                node depended upon that node
+                It is recommended to set `compile` to false when adding many nodes,
+                    and to manually run `.compile()` after they are added to prevent over-recompilation
+
+            Returns the passed node to allow chaining
+        '''
+        exists = node.name in self.nodes
+        if exists and (not replace):
+            raise nodes.NodeExistsError(f'Node with name {node.name!r} is already registered')
+        self.nodes[node.name] = node
+        if compile: self.compile(needed=exists)
+        return node
+    def pop_node(self, node: nodes.GrammarNode | str, *, ignore_missing: bool = False, compile: bool = True) -> nodes.GrammarNode:
+        '''
+            Removes and returns a node
+                Note that the node will still be bound to this `Grammar`
+
+            Raises `NodeMissingError` if the node is missing unless `ignore_missing`
+
+            If `compile`, then `.compile()` will be run after removing the node
+                Note that `.compile()` will not be run if the node is missing
+        '''
+        name = node if isinstance(node, str) else node.name
+        if name not in self.nodes:
+            if ignore_missing: return
+            raise nodes.NodeMissingError(f'Node {node!r} (name {name!r}) is missing')
+        node = self.pop(name)
+        if compile: self.compile()
+        return node
