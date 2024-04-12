e0aee17dc25c4ab09fcd97cf570c450eadb9f2db
Shae <shae.m.vc@proton.me>
2024-04-12 12:58:58 -0700

    Reworked compiler building and precompilation

--- src/caustic/lexer/compiler.py
+++ src/caustic/lexer/compiler.py
@@ -1,151 +1,127 @@
 #!/bin/python3
 
-'''
-    Uses `basic_compiler` and `./grammar.cag` to create a
-        more advanced/resilient grammar compiler
-'''
+'''Provides the `Compiler` class'''
 
 #> Imports
 import re
 import codecs
 import typing
-import warnings
 import buffer_matcher
 from pathlib import Path
-from functools import singledispatch
+from functools import singledispatchmethod
+from collections import abc as cabc
 
 from . import nodes
-from . import basic_compiler
+from . import util
 #</Imports
 
 #> Header >/
-__all__ = ('builtin_path', 'DEFAULT_GRAMMAR',
-           'compile_path', 'compile_file', 'compile_bytes', 'compile_buffermatcher', 'compile_dict',
-           'compile_node_name', 'compile_node')
+__all__ = ('Compiler',)
 
-builtin_path = Path(__file__).parent/'grammar.cag'
+class Compiler:
+    '''Compiles grammars'''
+    __slots__ = ('grammar', 'base_path')
 
-#DEFAULT_GRAMMAR_START
-if builtin_path.is_file():
-    DEFAULT_GRAMMAR = basic_compiler.compile(
-        buffer_matcher.DynamicBufferMatcher(builtin_path.read_bytes()))
-else:
-    warnings.warn(f'Builtin grammar file {builtin_path.as_posix()} does not exist')
-    DEFAULT_GRAMMAR = None
-#DEFAULT_GRAMMAR_END
+    grammar: dict[bytes, nodes.Node]
+    base_path: Path
 
-@singledispatch
-def compile(src: Path | typing.BinaryIO | bytes | buffer_matcher.SimpleBufferMatcher | dict, *,
-            source: Path | str | None = None, grammar: dict[bytes, nodes.Node] | None = None) -> dict[bytes, nodes.Node]:
-    '''Compiles a `Path`, file, `bytes`, or buffer-matcher into a grammar'''
-    raise TypeError(f'Cannot compile type of {bad}, must be a Path, file, bytes, or buffer-matcher')
+    def __init__(self, grammar: dict[bytes, nodes.Node],
+                 base_path: Path = Path(__file__).parent):
+        self.grammar = grammar
+        self.base_path = base_path
 
-@compile.register
-def compile_path(path: Path, *, source: Path | str | None = None,
-                 grammar: dict[bytes, nodes.Node] | None = None) -> dict[bytes, nodes.Node]:
-    '''Reads a path and compiles its contents'''
-    return compile_bytes(path.read_bytes(), source=(path if source is None else source), grammar=grammar)
-@compile.register
-def compile_file(file: typing.BinaryIO, source: Path | str | None = None,
-                 grammar: dict[bytes, nodes.Node] | None = None) -> dict[bytes, nodes.Node]:
-    '''Reads a binary file and compiles its contents'''
-    if source is None:
-        source = Path(file.name)
-        if not source.is_file(): source = file.name
-    return compile_bytes(file.read(), source=source, grammar=grammar)
+    # Compiling
+    @singledispatchmethod
+    def compile(self, src: Path | typing.BinaryIO | bytes | buffer_matcher.SimpleBufferMatcher | dict, *,
+                source: Path | str | None = None, bind: bool = True) -> dict[bytes, nodes.Node]:
+        '''Compiles grammars from various sources'''
+        raise TypeError(f'Cannot compile from type {type(src).__qualname__}')
 
-@compile.register
-def compile_bytes(data: bytes, *, source: Path | str | None = '<bytes>',
-                  grammar: dict[bytes, nodes.Node] | None = None) -> dict[bytes, nodes.Node]:
-    '''Compiles bytes'''
-    if source is None: source = '<bytes>'
-    return compile_buffermatcher(buffer_matcher.DynamicBufferMatcher(data), source=source, grammar=grammar)
-@compile.register
-def compile_buffermatcher(bm: buffer_matcher.SimpleBufferMatcher, source: Path | str | None = '<buffermatcher>',
-                          grammar: dict[bytes, nodes.Node] | None = None) -> dict[bytes, nodes.Node]:
-    '''Compiles a buffermatcher'''
-    if grammar is None: grammar = DEFAULT_GRAMMAR
-    if grammar is None:
-        raise FileNotFoundError('The default grammar did not exist at module import and as such can not be used')
-    grammars = {}
-    imports = {}; includes = {}
-    try:
-        while (bm.peek(1)):
-            while bm.match(nodes.WHITESPACE_PATT) or (grammar[b'COMMENT'](bm) is not nodes.Node.NO_RETURN): pass
-            if not bm.peek(1): break
-            if (p := grammar[b'PRAGMA'](bm)) is not nodes.Node.NO_RETURN:
-                match p['type']:
-                    case b'import' | b'include':
-                        f = Path(p['args'].decode())
-                        if not f.is_absolute():
-                            if isinstance(source, Path) and (nf := (source.parent / f)).is_file(): f = nf
-                            elif (nf := (Path.cwd() / f)).is_file(): f = nf
-                            elif (nf := (builtin_path.parent / f)).is_file(): f = nf
-                            else: raise FileNotFoundError(f'Cannot parse include: {f}')
-                        try: (includes if p['type'] == b'include' else imports).update(compile(f))
-                        except Exception as e:
-                            e.add_note(f'In {p["type"].decode()}')
-                            raise e
-                    case _ as t:
-                        raise TypeError(f'Unknown pragma type {t!r}')
+    ## Intermediate source types
+    @compile.register
+    def compile_path(self, path: Path, *, source: Path | str | None = None, **kwargs) -> dict[bytes, nodes.Node]:
+        '''
+            Compiles from a path
+            See `.compile()` or `.compile_buffermatcher()` for `kwargs`
+        '''
+        return self.compile_bytes(path.read_bytes(), source=path if source is None else source, **kwargs)
+    @compile.register
+    def compile_file(self, file: typing.BinaryIO, *, source: Path | str | None = None, **kwargs) -> dict[bytes, nodes.Node]:
+        '''
+            Compiles from a file
+            See `.compile()` or `.compile_buffermatcher()` for `kwargs`
+        '''
+        return self.compile_bytes(file.read(), source=file.name if source is None else source, **kwargs)
+    @compile.register
+    def compile_bytes(self, data: bytes, *, source: Path | str | None = None, **kwargs) -> dict[bytes, nodes.Node]:
+        '''
+            Compiles from bytes
+            See `.compile()` or `.compile_buffermatcher()` for `kwargs`
+        '''
+        return self.compile_buffermatcher(buffer_matcher.DynamicBufferMatcher(data),
+                                          source='<bytes>' if source is None else source, **kwargs)
+    @compile.register
+    def compile_buffermatcher(self, bm: buffer_matcher.SimpleBufferMatcher,
+                              source: Path | str | None = None, bind: bool = True,
+                              precompile_only: bool = False) -> dict[bytes, nodes.Node]:
+        '''Compiles a grammar (dict of names and nodes) from a buffer matcher'''
+        if source is None: source = '<buffermatcher>'
+        try: pre = self.pre_process(bm, source=source, bind=bind)
+        except Exception as e:
+            e.add_note(f'Whilst pre-processing grammar from: {source}')
+            raise e
+        if precompile_only: return pre
+        try: return self.post_process_compile(pre, bind=bind)
+        except Exception as e:
+            e.add_note(f'Whilst post-process compiling grammar from: {source}')
+            raise e
+
+    ## Pre-process
+    def pre_process(self, bm: buffer_matcher.SimpleBufferMatcher,
+                    source: Path | str | None = None) -> dict[bytes, dict]:
+        '''
+            Reads a buffer matcher and parses it into a format suitable for compilation
+            Also handles pragmas
+        '''
+        working = {}
+        while True:
+            # Discard junk characters and check for EOF
+            while bm.match(util.WHITESPACE_PATT) or self.grammar[b'COMMENT'](bm):
+                pass # ignore whitespace and comments
+            if not bm.peek(1): break # EOF
+            # Handle pragmas
+            if p := self.grammar[b'PRAGMA'](bm):
+                try: self.handle_pragma(p['type'], p['args'], working=working, bm=bm, source=source)
+                except Exception as e:
+                    e.add_note(f'In pragma at {bm.pos} ({bm.lno}:{bm.cno})')
+                    raise e
                 continue
-            stmt = grammar[b'STATEMENT'](bm)
-            if stmt is nodes.Node.NO_RETURN:
-                raise SyntaxError(f'Unexpected character: {c!r}')
-            grammars[stmt['name']] = stmt['expr']
-    except Exception as e:
-        e.add_note(f'Whilst parsing grammar from: {"<buffermatcher>" if source is None else source}')
-        e.add_note(f'At position {bm.pos} ({bm.lno+1}:{bm.cno})')
-        raise e
-    grammars = compile_dict(grammars, source=source, grammar=grammar) | includes
-    bind_nodes(grammars)
-    return grammars | imports
-@compile.register
-def compile_dict(data: dict, *, source: Path | str | None = '<dict>', grammar: dict[bytes, nodes.Node] = None) -> dict[bytes, nodes.Node]:
-    '''Compiles a dictionary of preprocessed tokens'''
-    try:
-        cnodes = {name: compile_node(b'group', expr) for name,expr in data.items()}
-    except Exception as e:
-        e.add_note(f'Whilst compiling grammar from: {"<dict>" if source is None else source}')
-        raise e
-    bind_nodes(cnodes)
-    return cnodes
+            # Parse statements
+            stmt = self.grammar[b'STATEMENT'](bm, stealer=True)
+            working[stmt['name']] = stmt['expr']
+        return working
+    ### Pragma
+    def handle_pragma(self, type_: bytes, args: bytes, *, working: dict[bytes, dict],
+                      bm: buffer_matcher.SimpleBufferMatcher, source: Path | str | None = None) -> None:
+        '''Handles a pragma statement during compilation'''
+        match type_:
+            case b'include':
+                if not args:
+                    raise SyntaxError('Include pragma requires args')
+                f = Path(args.decode())
+                if f.is_absolute():
+                    working.update(self.compile_path(f, bind=False, precompile_only=True))
+                    return
+                if isinstance(source, Path) and (nf := (source.parent / f)).is_file(): pass
+                elif (nf := (Path.cwd() / f)).is_file(): pass
+                elif (nf := (self.base_path / f)).is_file(): pass
+                else: raise FileNotFoundError(f'Cannot resolve relative file from include: {f}')
+                working.update(self.compile_path(nf, bind=False, precompile_only=True))
+            case _:
+                raise TypeError(f'Unknown pragma type {type_!r}')
 
-def compile_node_name(name: bytes | str | None, expr: dict) -> nodes.Node:
-    '''Compiles a node and names it'''
-    node = compile_node(**expr)
-    node.name = None if name is None else name if isinstance(name, str) else name.decode()
-    return node
-    
-def compile_node(type: typing.Literal[b'group', b'group_ws_sensitive', b'union', b'range', b'range_ws_sensitive',
-                                      b'string', b'pattern', 'stealer', b'context', b'noderef'], val: typing.Any | None = None) -> nodes.Node:
-    '''Compiles a single node (or recursive nodes) given its type and a value'''
-    match type:
-        case b'group':
-            return nodes.NodeGroup(*(compile_node_name(**n) for n in val))
-        case b'group_ws_sensitive':
-            return nodes.NodeGroup(*(compile_node_name(**n) for n in val), keep_whitespace=True)
-        case b'union':
-            return nodes.NodeUnion(*(compile_node_name(**n) for n in val))
-        case b'range':
-            return nodes.NodeRange(compile_node_name(**val['node']), int(val['min'] or 0), int(val['max']) if val['max'] else None)
-        case b'range_ws_sensitive':
-            return nodes.NodeRange(compile_node_name(**val['node']), int(val['min'] or 0), int(val['max']) if val['max'] else None, keep_whitespace=True)
-        case b'string':
-            return nodes.StringNode(codecs.escape_decode(val)[0])
-        case b'pattern':
-            flags = re.NOFLAG
-            for k,f in basic_compiler.RE_FLAGS.items():
-                if k in val['flags']: flags |= f
-            return nodes.PatternNode(re.compile(val['pattern'], flags), int(val['group']) if val['group'] else None)
-        case b'stealer':
-            return nodes.Stealer()
-        case b'context':
-            ctx = val.get('raw', None)
-            if ctx is None:
-                ctx = codecs.escape_decode(val['str'])[0]
-            return nodes.Context(ctx)
-        case b'noderef':
-            return nodes.NodeRef(val)
-        case _:
-            raise TypeError(f'Unknown node type {type!r}')
+    ## Compile
+    def post_process_compile(working: dict[bytes, dict], *, bind: bool = True) -> dict[bytes, nodes.Node]:
+        '''Compiles pre-processed grammar into nodes'''
+        for k,v in working.items():
+            print(k, v)
